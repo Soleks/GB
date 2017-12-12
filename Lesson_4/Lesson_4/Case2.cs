f0c1585bac9fe60c2a8338087414a1b33f66616a
Soleks <SolekX@gmail.com>
2017-12-12 15:23:29 +0300

    Case2

---
+++ Lesson_4/Lesson_4/Case2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    class Case2
+    {
+        // Создание массива и заполнение его значениями с шагом step
+        public Case2(int n, int step)
+        {
+            array_ = new int[n];
+            for (int i = 0; i < n; i++)
+                array_[i] = i * step;
+        }
+        // Создание массива и заполнение его случайными числами от min до max
+        public Case2(int n, int min, int max)
+        {
+            array_ = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                array_[i] = rnd.Next(min, max);
+        }
+
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+
+                if (array_.Length == 0)
+                {
+                    return sum;
+                }
+
+                for (int i = 0; i < array_.Length; i++)
+                {
+                    sum += array_[i];
+                }
+
+                return sum;
+            }
+        }
+
+        public void Inverse()
+        {
+            for (int i = 0; i < array_.Length; i++)
+            {
+                array_[i] *= -1;
+            }
+        }
+
+        public void Multiplication(int multiplier)
+        {
+            for (int i = 0; i < array_.Length; i++)
+            {
+                array_[i] *= multiplier;
+            }
+        }
+
+        public int MaxCount
+        {
+            get
+            {
+                int count = 0;
+
+                for (int i = 0; i < array_.Length; i++)
+                {
+                    if (array_[i] == Max)
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+        }
+
+        public int Max
+        {
+            get
+            {
+                int max = array_[0];
+                for (int i = 1; i < array_.Length; i++)
+                    if (array_[i] > max) max = array_[i];
+                return max;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                int min = array_[0];
+                for (int i = 1; i < array_.Length; i++)
+                    if (array_[i] < min) min = array_[i];
+                return min;
+            }
+        }
+        public int CountPositiv
+        {
+            get
+            {
+                int count = 0;
+
+            for (int i = 0; i < array_.Length; i++)
+                    if (array_[i] > 0) count++;
+                return count;
+            }
+        }
+        public override string ToString()
+        {
+            string s = "";
+            foreach (int v in array_)
+                s = s + v + " ";
+            return s;
+        }
+
+        private int[] array_;
+    }
+}
--- Lesson_4/Lesson_4/Program.cs
+++ Lesson_4/Lesson_4/Program.cs
@@ -18,10 +18,39 @@ namespace Lesson_4
                 В данной задаче под парой подразумевается два подряд идущих элемента массива.
                 Например, для массива из пяти элементов: 6; 2; 9; –3; 6 – ответ: 4.
             */
+
+            Console.WriteLine("Case 1");
+            Console.WriteLine();
             Case1 case1 = new Case1(20, -1000, 1000);
 
             case1.ShowArray();
             case1.NumberOfPairs();
+
+            /*
+                 2. а) Дописать класс для работы с одномерным массивом. Реализовать конструктор, 
+                 создающий массив заданной размерности и заполняющий массив числами от начального 
+                 значения с заданным шагом. Создать свойство Sum, которые возвращают сумму элементов
+                 массива, метод Inverse, меняющий знаки у всех элементов массива, Метод Multi, 
+                 умножающий каждый элемент массива на определенное число, 
+                 свойство MaxCount, возвращающее количество максимальных элементов.
+                 В Main продемонстрировать работу класса.
+             */
+
+            Case2 case2 = new Case2(10, 2);
+            Console.WriteLine();
+            Console.WriteLine("Case 2");
+
+            Console.WriteLine("array: " + case2.ToString());
+            Console.WriteLine("Sum elements: " + case2.Sum);
+
+            case2.Multiplication(10);
+            Console.WriteLine("Multiplication: " + case2.ToString());
+
+            case2.Inverse();
+            Console.WriteLine("Inverse: " + case2.ToString());
+
+            Console.WriteLine("Max: " + case2.Max);
+            Console.WriteLine("MaxCount: " + case2.MaxCount);
 
             Console.ReadKey();
         }
