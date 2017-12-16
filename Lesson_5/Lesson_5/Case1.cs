d603616f8e1c20e48d9096f14b64f55b3dd5f6b6
Soleks <SolekX@gmail.com>
2017-12-16 13:49:16 +0300

    Lesson_5

---
+++ Lesson_5/Lesson_5/Case1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    class Case1
+    {
+        public void CheckLogin(string login)
+        {
+            if (Char.IsNumber(login[0]) || 
+                (login.Length < 1 || login.Length > 10))
+            {
+                Console.WriteLine("Wrong login");
+
+                return;
+            }
+
+            Console.WriteLine("login success");
+        }
+
+        public void CheckLoginReg(string login)
+        {
+            Regex reg = new Regex(regex_);
+
+            Console.WriteLine((reg.IsMatch(login) ? "login success" : "Wrong login"));
+        }
+
+        private readonly string regex_ = "^[a-zA-Z]{2,10}";
+    }
+}
---
+++ Lesson_5/Lesson_5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Урок 5 
+ Олексенко С.С
+*/
+namespace Lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Создать программу, которая будет проверять 
+             корректность ввода логина. Корректным логином 
+             будет строка от 2 - х до 10 - ти символов, 
+             содержащая только буквы или цифры, и при этом 
+             цифра не может быть первой.
+             */
+
+            Case1 case1 = new Case1();
+
+            //а) без использования регулярных выражений;
+            case1.CheckLogin("1qwert");
+            case1.CheckLogin("qwert");
+
+            //б) **с использованием регулярных выражений.
+
+            case1.CheckLoginReg("1qwert");
+            case1.CheckLoginReg("qwert");
+
+            //2.Разработать методы для решения следующих задач.
+            //Дано сообщение:
+
+
+            Console.ReadKey();
+        }
+    }
+}
